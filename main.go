96ed985858f9d1a87a9ce9588e0d90fd5a6333bc
Toan Nguyen <hgiasac@gmail.com>
2024-03-10 14:00:38 +0700

    Add request timeout (#2)
    * add request timeout

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var cli struct {
 }
 
 func main() {
-	cmd := kong.Parse(&cli)
+	cmd := kong.Parse(&cli, kong.UsageOnError())
 	switch cmd.Command() {
 	case "convert":
 		command.ConvertToNDCSchema(&cli.Convert)
