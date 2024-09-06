d73754cb2181ae3746b3d14fd21eb3aed0a8ab70
Toan Nguyen <hgiasac@gmail.com>
2024-09-07 00:09:27 +0700

    feat: tls config for server

--- schema/setting.go
+++ schema/setting.go
@@ -101,16 +101,49 @@ func (rs RetryPolicySetting) Validate() error {
 	return nil
 }
 
+// TLSConfig represents the transport layer security (LTS) configuration for the HTTP client
+type TLSConfig struct {
+	// Path to the TLS cert to use for TLS required connections.
+	CertFile string `json:"certFile,omitempty" yaml:"certFile,omitempty" mapstructure:"certFile"`
+	// Alternative to cert_file. Provide the certificate contents as a string instead of a filepath.
+	CertPem string `json:"certPem,omitempty" yaml:"certPem,omitempty" mapstructure:"certPem"`
+	// Path to the TLS key to use for TLS required connections.
+	KeyFile string `json:"keyFile,omitempty" yaml:"keyFile,omitempty" mapstructure:"keyFile"`
+	// Alternative to key_file. Provide the key contents as a string instead of a filepath.
+	KeyPem string `json:"keyPem,omitempty" yaml:"keyPem,omitempty" mapstructure:"keyPem"`
+	// Path to the CA cert. For a client this verifies the server certificate. For a server this verifies client certificates.
+	// If empty uses system root CA.
+	CAFile string `json:"caFile,omitempty" yaml:"caFile,omitempty" mapstructure:"caFile"`
+	// Alternative to ca_file. Provide the CA cert contents as a string instead of a filepath.
+	CAPem string `json:"caPem,omitempty" yaml:"caPem,omitempty" mapstructure:"caPem"`
+	// Additionally you can configure TLS to be enabled but skip verifying the server's certificate chain.
+	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty" yaml:"insecureSkipVerify,omitempty" mapstructure:"insecureSkipVerify"`
+	// Whether to load the system certificate authorities pool alongside the certificate authority.
+	IncludeSystemCACertsPool bool `json:"includeSystemCACertsPool,omitempty" yaml:"includeSystemCACertsPool,omitempty" mapstructure:"includeSystemCACertsPool"`
+	// Minimum acceptable TLS version.
+	MinVersion string `json:"minVersion,omitempty" yaml:"minVersion,omitempty" mapstructure:"minVersion"`
+	// Maximum acceptable TLS version.
+	MaxVersion string `json:"maxVersion,omitempty" yaml:"maxVersion,omitempty" mapstructure:"maxVersion"`
+	// Explicit cipher suites can be set. If left blank, a safe default list is used.
+	// See https://go.dev/src/crypto/tls/cipher_suites.go for a list of supported cipher suites.
+	CipherSuites []string `json:"cipherSuites,omitempty" yaml:"cipherSuites,omitempty" mapstructure:"cipherSuites"`
+	// Specifies the duration after which the certificate will be reloaded. If not set, it will never be reloaded.
+	// The interval unit is minute
+	ReloadInterval uint `json:"reloadInterval,omitempty" yaml:"reloadInterval,omitempty" mapstructure:"reloadInterval"`
+}
+
 // ServerConfig contains server configurations
 type ServerConfig struct {
 	URL     EnvString            `json:"url" yaml:"url" mapstructure:"url"`
-	ID      string               `json:"id,omitempty" yaml:"id,omitempty" mapstructure:"group"`
+	ID      string               `json:"id,omitempty" yaml:"id,omitempty" mapstructure:"id"`
 	Headers map[string]EnvString `json:"headers,omitempty" yaml:"headers,omitempty" mapstructure:"headers"`
 	// configure the request timeout in seconds, default 30s
 	Timeout         *EnvInt                   `json:"timeout,omitempty" yaml:"timeout,omitempty" mapstructure:"timeout"`
 	Retry           *RetryPolicySetting       `json:"retry,omitempty" yaml:"retry,omitempty" mapstructure:"retry"`
 	SecuritySchemes map[string]SecurityScheme `json:"securitySchemes,omitempty" yaml:"securitySchemes,omitempty" mapstructure:"securitySchemes"`
 	Security        AuthSecurities            `json:"security,omitempty" yaml:"security,omitempty" mapstructure:"security"`
+	// The transport layer security (LTS) configuration for the HTTP client to communicate the current server
+	TLS *TLSConfig `json:"tls,omitempty" yaml:"tls,omitempty" mapstructure:"tls"`
 }
 
 // Validate if the current instance is valid
